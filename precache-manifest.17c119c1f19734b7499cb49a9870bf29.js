f138ee7617dbed3ca0c326f6cb9c4c2b7bae361a
Jeisson Serpa <jeisson-serpa@outlook.com>
2020-08-16 15:20:00 -0500

    Updates

---
+++ precache-manifest.17c119c1f19734b7499cb49a9870bf29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ab01ac395a3d42dc82e56638842897",
+    "url": "/noticias/index.html"
+  },
+  {
+    "revision": "c0caae9c19f29b54cde2",
+    "url": "/noticias/static/css/main.de8479e4.chunk.css"
+  },
+  {
+    "revision": "ba400854885716a3adec",
+    "url": "/noticias/static/js/2.7448b09c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/noticias/static/js/2.7448b09c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0caae9c19f29b54cde2",
+    "url": "/noticias/static/js/main.81d1d125.chunk.js"
+  },
+  {
+    "revision": "12b7275d294335a4a565",
+    "url": "/noticias/static/js/runtime-main.23049d33.js"
+  }
+]);
